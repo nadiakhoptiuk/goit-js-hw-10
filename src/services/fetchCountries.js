3bca2a7d88b8f24e75c5e392b0e75a51fe94acb3
Nadia Khoptiuk <holineinadia@gmail.com>
2022-06-09 23:23:40 +0300

    update fetch

--- src/services/fetchCountries.js
+++ src/services/fetchCountries.js
@@ -1,9 +1,9 @@
 const options = `?fields=name,capital,population,flags,languages`;
-const url = 'https://restcountries.com/v3.1/name/';
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
 // функція, яка забирає дані з бекенду
 function fetchCountries(name) {
-  return fetch(`${url}${name}${options}`).then(response => {
+  return fetch(`${BASE_URL}${name}${options}`).then(response => {
     if (!response.ok) {
       throw new Error('Oops, there is no country with that name');
     }
